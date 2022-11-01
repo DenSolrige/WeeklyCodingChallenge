0174a11437057632663520d07e5a36f9fef4c434
Sunny <faronwmoore@gmail.com>
2022-11-01 10:31:22 -0500

    Initial commit

--- Oct31st/src/AtBash.java
+++ Oct31st/src/AtBash.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class AtBash {
+import java.util.stream.Collectors;
+
+public class AtBash {
+
+    public static String atbash(String input){
+        return input.chars().mapToDouble(c->(double)c).map(c ->
+                (int)(3/((((c-65)-12.5)/13.0*((c-65)-12.5)/13.0)+2))*(2*'A'+25-c)
+                +(int)(3/((((c-97)-12.5)/13.0*((c-97)-12.5)/13.0)+2))*(2*'a'+25-c)
+                +(1-(int)(3/((((c-65)-12.5)/13.0*((c-65)-12.5)/13.0)+2))-(int)(3/((((c-97)-12.5)/13.0*((c-97)-12.5)/13.0)+2)))*c)
+                .mapToObj(c -> Character.valueOf((char)c).toString())
+                .collect(Collectors.joining());
+    }
+
+    public static void main(String[] args) {
+        //Rather math-y, wanted to try making it without conditionals
+        System.out.println(atbash("apple"));
+        System.out.println(atbash("Hello world!"));
+        System.out.println(atbash("Christmas is the 25th of December"));
+    }
 }
