269dad12e3116bd113ef2c2e41c3a7e5444ed3fe
Sunny <faronwmoore@gmail.com>
2022-11-01 11:05:03 -0500

    Cleaned up the expressions

--- Oct31st/src/AtBash.java
+++ Oct31st/src/AtBash.java
@@ -4,13 +4,13 @@ public class AtBash {
 
     public static String atbash(String input){
         return input.chars().mapToDouble(c->(double)c).map(c ->
-                (int)(3/((((c-65)-12.5)/13.0*((c-65)-12.5)/13.0)+2)) // selects uppercase letters
-                        *(2*'A'+25-c)  // and reverses them
-                +(int)(3/((((c-97)-12.5)/13.0*((c-97)-12.5)/13.0)+2)) // selects lowercase letters
-                        *(2*'a'+25-c) // and reverses them
+                (int)(3/((((c-'A')-12.5)/13.0*((c-'A')-12.5)/13.0)+2)) // selects uppercase letters
+                        *('Z'-c+'A')  // and reverses them
+                +(int)(3/((((c-'a')-12.5)/13.0*((c-'a')-12.5)/13.0)+2)) // selects lowercase letters
+                        *('z'-c+'a') // and reverses them
                 +(1
-                   -(int)(3/((((c-65)-12.5)/13.0*((c-65)-12.5)/13.0)+2)) // deselects uppercase letters,
-                   -(int)(3/((((c-97)-12.5)/13.0*((c-97)-12.5)/13.0)+2)) // deselects lowercase letters,
+                   -(int)(3/((((c-'A')-12.5)/13.0*((c-'A')-12.5)/13.0)+2)) // deselects uppercase letters,
+                   -(int)(3/((((c-'a')-12.5)/13.0*((c-'a')-12.5)/13.0)+2)) // deselects lowercase letters,
                 )*c) // and leaves non-letters unaffected
                 .mapToObj(c -> Character.valueOf((char)c).toString())
                 .collect(Collectors.joining());
