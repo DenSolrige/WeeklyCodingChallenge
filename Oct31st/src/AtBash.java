8c0a806476a09f30119458e90e51e2bf10e7a1c1
Sunny <faronwmoore@gmail.com>
2022-11-01 10:34:33 -0500

    Added some comments

--- Oct31st/src/AtBash.java
+++ Oct31st/src/AtBash.java
@@ -4,9 +4,12 @@ public class AtBash {
 
     public static String atbash(String input){
         return input.chars().mapToDouble(c->(double)c).map(c ->
-                (int)(3/((((c-65)-12.5)/13.0*((c-65)-12.5)/13.0)+2))*(2*'A'+25-c)
-                +(int)(3/((((c-97)-12.5)/13.0*((c-97)-12.5)/13.0)+2))*(2*'a'+25-c)
-                +(1-(int)(3/((((c-65)-12.5)/13.0*((c-65)-12.5)/13.0)+2))-(int)(3/((((c-97)-12.5)/13.0*((c-97)-12.5)/13.0)+2)))*c)
+                (int)(3/((((c-65)-12.5)/13.0*((c-65)-12.5)/13.0)+2))*(2*'A'+25-c) // selects uppercase letters and reverses them
+                +(int)(3/((((c-97)-12.5)/13.0*((c-97)-12.5)/13.0)+2))*(2*'a'+25-c) // selects lowercase letters and reverses them
+                +(1
+                   -(int)(3/((((c-65)-12.5)/13.0*((c-65)-12.5)/13.0)+2))
+                   -(int)(3/((((c-97)-12.5)/13.0*((c-97)-12.5)/13.0)+2))
+                )*c) // non-letters unaffected (inverse of the previous two selections)
                 .mapToObj(c -> Character.valueOf((char)c).toString())
                 .collect(Collectors.joining());
     }
